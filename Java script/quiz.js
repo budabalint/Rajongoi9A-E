01b0789a3f0d8893453a57ba089ef35bdc12d0dd
gocsikdavid <gocsik.david@students.jedlik.eu>
2025-02-06 17:03:37 +0100

    jedlik quiz befejezés hozzáadva

--- Java script/quiz.js
+++ Java script/quiz.js
@@ -7,8 +7,8 @@ const questions = [
     { q: "Melyik épületben található Konyha?", options: ["A Épület", "B Épület", "Mindkettő", "Egyik sem"], answer: 0 },
     { q: "Mikor alkotta a szódavizet?", options: ["1824", "1825", "1826", "1827"], answer: 2 },
     { q: "Hol készültek a zöld csempék?", options: ["Kalocsa", "Budapest", "Herend", "Zsolna"], answer: 3 },
-    { q: "?", options: ["Kék", "Átlátszó", "Zöld", "Fehér"], answer: 1 },
-    { q: "?", options: ["10", "12", "14", "16"], answer: 1 }
+    { q: "Mi nem található a jedlikben?", options: ["wc papír", "könyvtár", "büfé", "ketrec"], answer: 0 },
+    { q: "Mi volt Jedlik Ányos eredeti keresztneve?", options: ["Áron", "István", "Péter", "Rezső"], answer: 1 }
 ];
 
 let score = 0;
